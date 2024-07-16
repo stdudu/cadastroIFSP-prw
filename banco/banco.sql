b42fb88551b05e472fe5b907b4c551d87350da3a
DU2 <nicolalmeida_se@outlook.com>
2024-07-16 09:28:25 -0300

    changes

--- banco/banco.sql
+++ banco/banco.sql
@@ -2,14 +2,14 @@ drop database if exists IFSP;
 create database if not exists IFSP;
 use IFSP;
 
---CRIANDO AS TABELAS DO BD
---criando tabela cidade
+-- CRIANDO AS TABELAS DO BD
+-- criando tabela cidade
 create table cidade(
     id int primary key auto_increment,
     nome varchar(50),
-    estado varchar(2),
+    estado varchar(2)
 );
---criando tabela cliente
+-- criando tabela cliente
 create table cliente(
     cpf int primary key auto_increment,
     nome varchar(50),
@@ -19,4 +19,4 @@ create table cliente(
     id_cidade int,
     constraint fk_clientecidade foreign key(id_cidade) references cidade(id)
 );
---TABELAS CRIADAS
+-- TABELAS CRIADAS
--- listarcidade.php
+++ listarcidade.php
@@ -9,7 +9,7 @@
 <body style="background-color: azure;">
     <?php
 
-    include("includes/conexao.php");
+    include("include/conexao.php");
 
     $sql = "SELECT * FROM cidade";
     //executa consulta
